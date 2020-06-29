aaabc7c146fe9f155240ce6e5733916e666a927e
rajupotharaju155 <rajupotharaju155@gmail.com>
2020-06-29 13:25:30 +0530

    Create campaign UI fixed

---
+++ lib/model/camp.dart
@@ -0,0 +1,7 @@
+class Campaign{
+  final String campName;
+  final String dueDate;
+  final int target;
+  final int volenteers;
+  Campaign({this.campName, this.dueDate, this.target, this.volenteers});
+}
